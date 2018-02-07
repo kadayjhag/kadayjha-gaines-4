a5eb42e291ac079833a2b3f3dc5ee5e0e54fd197
SCSFMAC10 <scsfmac10@Admins-MacBook-Air.local>
2018-02-06 17:01:36 -0800

    updated site

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
+
+app.get('/aboutme', (req, res) => {
+  res.sendFile(__dirname + '/aboutme.html')
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/contact.html')
+})
+
 
 app.listen(process.env.PORT || 3000, () => console.log('LIVE on 3000'))
